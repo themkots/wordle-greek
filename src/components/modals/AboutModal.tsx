6e019f90cff4d43f4818b2103e60977c6b69e827
gvarnavi <gvarnavi@mit.edu>
2022-01-26 14:05:42 -0500

    credited original repo

--- src/components/modals/AboutModal.tsx
+++ src/components/modals/AboutModal.tsx
@@ -9,20 +9,27 @@ export const AboutModal = ({ isOpen, handleClose }: Props) => {
   return (
     <BaseModal title="Πληροφορίες" isOpen={isOpen} handleClose={handleClose}>
       <p className="text-sm text-gray-500">
-	Η ιστοσελίδα αυτή είναι ένα open source clone του παιχνιδίου Wordle -{' '}
+	Η ιστοσελίδα αυτή είναι ένα clone του παιχνιδίου Wordle.{' '}
         <a
           href="https://github.com/gvarnavi/wordle-greek"
           className="underline font-bold"
         >
-          Δες τον κώδικα εδώ
+          Δες τον κώδικα
         </a>{' '}
         και{' '}
         <a
           href="https://www.powerlanguage.co.uk/wordle/"
           className="underline font-bold"
         >
-          παίξε το original εδώ
-        </a>
+          παίξε το original εδώ.
+        </a>{' '}
+	 Βασισμένο στο{' ' }
+	<a
+	 href="https://github.com/hannahcode/wordle"
+	 className="underline font-bold"
+	>
+	  open source clone εδώ.
+	</a>
       </p>
     </BaseModal>
   )
